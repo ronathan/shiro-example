82db13e748fe00e9c2593b066f7c33daba168edc
Ronald Victorino <ron@nthgensoftware.com>
2013-10-08 16:57:15 -0400

    Used shiro quick-start, changed Bootstrap.groovy to create users, added home controller, added security filters

---
+++ grails-app/controllers/com/example/HomeController.groovy
@@ -0,0 +1,16 @@
+package com.example
+
+class HomeController {
+
+    def index() {
+        render "This page is not secured"
+    }
+
+    def secured() {
+        render "This page requires a user to be logged in"
+    }
+
+    def admin() {
+        render "This page requires the logged in uesr to be an administrator"
+    }
+}
